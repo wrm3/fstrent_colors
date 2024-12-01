eb5353c1fddfdc2dd806f123df339933a13cd8c2
wrm3 <wrmartel3@gmail.com>
2024-12-01 18:22:16 -0500

    Bump version: 1.0.8 → 1.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fstrent_colors",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_packages(),
     install_requires=[
         "colorama",
