1679fe0862df4442f870cefa39161c10407410ee
wrm3 <wrmartel3@gmail.com>
2024-12-01 16:15:51 -0500

    updated...

--- gh_push.bat
+++ gh_push.bat
@@ -2,9 +2,15 @@
 set /p BUMP_TYPE="Enter bump type (major/minor/patch): "
 set /p MESSAGE="Enter commit message: "
 
+:: Git commands
+git add .
+git commit -m "%MESSAGE%"
+
 :: Run bumpversion
 bumpversion %BUMP_TYPE%
 
 :: Git commands (bumpversion already does the commit and tag)
+git add .
+git commit -m "%MESSAGE%"
 git push origin main
 git push origin --tags
