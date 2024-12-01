487cb553e795d1834afe5fba31f2d0af8ae51e1f
wrm3 <wrmartel3@gmail.com>
2024-12-01 08:22:24 -0500

    Bump version: 1.0.0 → 1.0.1

--- fstrent_colors.py
+++ fstrent_colors.py
@@ -2,7 +2,7 @@
 # Description
 #<=====>#
 """FSTrent Colors - Enhanced colors options for terminal output."""
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 #<=====>#
 # Imports
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fstrent_colors",
-    version="1.0.0",
+    version="1.0.1",
     author="Warren R Martel III",
     author_email="wrmartel3@gmail.com",
     description="enhanced colors options for terminal output",
